76f431efa0e05e97ac692360cbd1b119798ad6e1
charlie <xiaoxw@tedu.cn>
2024-10-09 16:49:07 +0800

    DAY02-PM 04 jwt创建 和jwt解析测试

--- lbs-basic/basic-web/src/test/java/com/tarena/basic/test/jwt/JwtRunTest.java
+++ lbs-basic/basic-web/src/test/java/com/tarena/basic/test/jwt/JwtRunTest.java
@@ -1,8 +1,10 @@
 package com.tarena.basic.test.jwt;
 
+import com.tarena.lbs.base.protocol.exception.BusinessException;
 import com.tarena.lbs.common.passport.encoder.JwtEncoder;
 import com.tarena.lbs.pojo.basic.po.AdminPO;
 import org.junit.Before;
+import org.junit.Test;
 
 public class JwtRunTest {
     //将jwt工具代码封装了一个 编码器
@@ -19,5 +21,34 @@ public class JwtRunTest {
     }
     //在项目中使用这个工具类 做2个工作
     //1.使用AdminPO对象 生成jwt
+    @Test
+    public void testGenerateToken() throws Exception {
+        //1.准备数据
+        AdminPO po = new AdminPO();
+        po.setId(1);
+        po.setAccountPhone("123456789");
+        po.setNickname("tarena");
+        po.setEmail("tarena@qq.com");
+        po.setBusinessId(1);
+        po.setAccountStatus(1);
+        po.setAccountType(1);
+        //2. 调用工具api
+        String jwt = jwtEncoder.generateToken(po);
+        System.out.println(jwt);
+    }
     //2.拿到一个jwt 反向解析AdminPO
+    @Test
+    public void testTokenParsePO() throws BusinessException {
+        String jwt="eyJhbGciOiJIUzUxMiJ9.eyJwYXlsb2FkIjoie1wiYWNjb3VudFBob25lXCI6XCIxMjM0NTY3ODlcIixcImFjY291bnRTdGF0dXNcIjoxLFwiYWNjb3VudFR5cGVcIjoxLFwiYnVzaW5lc3NJZFwiOjEsXCJlbWFpbFwiOlwidGFyZW5hQHFxLmNvbVwiLFwiaWRcIjoxLFwibmlja25hbWVcIjpcInRhcmVuYVwifSJ9.6XzJ_XVa-YPPq4PRqpQMMn0DkR12-dqFH_Q45DuEuOuor-kQtiL-Rl0uIjwOF7Rz5UddHs2KYmGvBoS8hDy3Uw";
+        //调用api 解析
+        AdminPO po = jwtEncoder.getLoginFromToken(jwt, AdminPO.class);
+        System.out.println(po);
+    }
+
+
+
+
+
+
+
 }
