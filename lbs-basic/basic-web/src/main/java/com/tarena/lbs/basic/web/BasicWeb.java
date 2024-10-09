97bf61fe5d6252f2ad97e3b23068ef30b12bdd00
charlie <xiaoxw@tedu.cn>
2024-10-09 17:31:00 +0800

    DAY02-PM 06 创建网关 启动前端测试

--- lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/BasicWeb.java
+++ lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/BasicWeb.java
@@ -1,9 +1,11 @@
 package com.tarena.lbs.basic.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tarena.lbs.basic.web.mapper")
 public class BasicWeb {
     public static void main(String[] args) {
         SpringApplication.run(BasicWeb.class, args);
