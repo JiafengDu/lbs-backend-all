f93a4db7cdb4ccf00ba908aff17e903b9db38e4f
charlie <xiaoxw@tedu.cn>
2024-10-09 17:47:12 +0800

    DAY02-PM 06 创建网关 启动前端测试

---
+++ lbs-gateway/src/main/java/com/tarena/lbs/gateway/GatewayStart.java
@@ -0,0 +1,11 @@
+package com.tarena.lbs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayStart {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStart.class, args);
+    }
+}
