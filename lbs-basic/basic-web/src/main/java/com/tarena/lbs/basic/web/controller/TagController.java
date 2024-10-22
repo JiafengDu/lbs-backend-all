4c7a6f9cac5ce410c49e1aa98cf4fbe0305e5960
charlie <xiaoxw@tedu.cn>
2024-10-22 11:35:13 +0800

    DAY12-AM 06 小程序查询 用户标签

--- lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/controller/TagController.java
+++ lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/controller/TagController.java
@@ -17,11 +17,18 @@ import java.util.Map;
 public class TagController {
     @Autowired
     private TagService tagService;
-
+    //文章标签 查询 一级标签名字是key 二级标签 列表对象是value
     @GetMapping("/basic/tagLibrary/info/article/tags")
     public Result<Map<String, List<TagVO>>> getArticleTags(){
         //考虑 既然标签结构 文章和用户相同 后续还会查询用户标签 tagType=0|1
         Map<String,List<TagVO>> tags=tagService.getTagsByType(0);
+        return new Result<>(tags);
+    }
+    //用户标签 和文章标签数据结构 查询逻辑完全相同 只是入参传递的是tag_type=1
+    @GetMapping("/basic/tagLibrary/info/userList")
+    public Result<Map<String, List<TagVO>>> getUserTags(){
+        //考虑 既然标签结构 文章和用户相同 后续还会查询用户标签 tagType=0|1
+        Map<String,List<TagVO>> tags=tagService.getTagsByType(1);
         return new Result<>(tags);
     }
 }
