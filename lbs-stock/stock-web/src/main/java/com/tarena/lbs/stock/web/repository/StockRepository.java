d9f0146c5904a2aa51a21692355a6337cda7cf34
charlie <xiaoxw@tedu.cn>
2024-10-23 15:44:04 +0800

    DAY13-PM 05 stock实现接口方法 初始化库存 幂等设计

--- lbs-stock/stock-web/src/main/java/com/tarena/lbs/stock/web/repository/StockRepository.java
+++ lbs-stock/stock-web/src/main/java/com/tarena/lbs/stock/web/repository/StockRepository.java
@@ -1,5 +1,7 @@
 package com.tarena.lbs.stock.web.repository;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.tarena.lbs.pojo.stock.po.CouponStockPO;
 import com.tarena.lbs.stock.web.mapper.StockMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,4 +10,15 @@ import org.springframework.stereotype.Repository;
 public class StockRepository {
     @Autowired
     private StockMapper stockMapper;
+
+    public Long countStockByCouponId(Integer couponId) {
+        //select count(0) from coupon_stock where coupon_id=#{}
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("coupon_id", couponId);
+        return stockMapper.selectCount(queryWrapper);
+    }
+
+    public int save(CouponStockPO poParam) {
+        return stockMapper.insert(poParam);
+    }
 }
--- lbs-stock/stock-web/src/main/java/com/tarena/lbs/stock/web/service/StockService.java
+++ lbs-stock/stock-web/src/main/java/com/tarena/lbs/stock/web/service/StockService.java
@@ -1,16 +1,35 @@
 package com.tarena.lbs.stock.web.service;
 
+import com.tarena.lbs.base.common.utils.Asserts;
+import com.tarena.lbs.base.protocol.exception.BusinessException;
 import com.tarena.lbs.pojo.stock.param.CouponStockParam;
+import com.tarena.lbs.pojo.stock.po.CouponStockPO;
 import com.tarena.lbs.stock.web.repository.StockRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class StockService {
     @Autowired
     private StockRepository stockRepository;
 
-    public Boolean initStock(CouponStockParam param) {
-        return null;
+    public Boolean initStock(CouponStockParam param) throws BusinessException {
+        //封装一下po 写入数据库 如果考虑幂等 一个couponId只能存在一个 stock库存数据
+        //先查 在增 外部包裹一个锁
+        Long count=stockRepository.countStockByCouponId(param.getCouponId());
+        Asserts.isTrue(count>0,new BusinessException("-2","库存已经初始化过了"));
+        //没有库存 组织一个poparam数据 写入数据库
+        CouponStockPO poParam=new CouponStockPO();
+        BeanUtils.copyProperties(param,poParam);
+        //状态 时间 createAt updateAt
+        poParam.setCreateAt(new Date());
+        poParam.setUpdateAt(new Date());//不太严谨
+        int result=stockRepository.save(poParam);
+        //mysql每次写操作 都会反馈写操作影响的行数 如果是新增 增几条就返回几 如果增一条成了就返回1 败了没异常 返回0
+        //mybatis框架是可以在增删改的方法上 接收返回值
+        return result>0;
     }
 }
