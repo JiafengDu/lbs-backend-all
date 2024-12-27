f27cbc2632de6891c0caaf40cef7987fc7fe126b
JiafengDu <49822677+JiafengDu@users.noreply.github.com>
2024-12-26 20:16:58 -0800

    completed businessRepository's assembleBusinessQuery function

--- lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/repository/BusinessRepository.java
+++ lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/repository/BusinessRepository.java
@@ -1,10 +1,12 @@
 package com.tarena.lbs.basic.web.repository;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.tarena.lbs.basic.web.mapper.BusinessMapper;
 import com.tarena.lbs.pojo.basic.po.BusinessPO;
 import com.tarena.lbs.pojo.basic.query.BusinessQuery;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -32,5 +34,44 @@ public class BusinessRepository {
         List<BusinessPO> pos = businessMapper.selectList(null);
         PageInfo<BusinessPO> pageInfo = new PageInfo<>(pos);
         return pageInfo;
+    }
+
+    public PageInfo<BusinessPO> getPages(BusinessQuery query) {
+        // 1. use the nonempty params in query as the where clause in the sql statement
+        QueryWrapper<BusinessPO> queryWrapper = assembleBusinessQuery(query);
+        // 2. start PageHelper
+        PageHelper.startPage(query.getPageNo(), query.getPageSize());
+        // 3. sql immediately after PageHelper to ensure thread safety
+        List<BusinessPO> pos = businessMapper.selectList(queryWrapper);
+        return new PageInfo<>(pos);
+    }
+
+    private QueryWrapper<BusinessPO> assembleBusinessQuery(BusinessQuery query) {
+        QueryWrapper<BusinessPO> queryWrapper = new QueryWrapper<>();
+        // 1. businessName not null -> where business_name like (%#{}%)
+        if (StringUtils.isNotBlank(query.getBusinessName())) {
+            queryWrapper.like("business_name", query.getBusinessName());
+        }
+        // 2. businessHeadPhone not null -> where business_head_phone=#{}
+        if (StringUtils.isNotBlank(query.getBusinessHeadPhone())) {
+            queryWrapper.eq("business_head_phone", query.getBusinessHeadPhone());
+        }
+        // 3. businessStatus 0,1,2,3,4,5 -> business_status=#{}
+        if ((query.getBusinessStatus()!=null)) {
+            queryWrapper.eq("business_status", query.getBusinessStatus());
+        }
+        // 4. startTime not null -> entry_time>#{}
+        if (query.getStartingTime()!=null) {
+            queryWrapper.gt("entry_time", query.getStartingTime());
+        }
+        // 5. endTime not null -> entry_time<#{}
+        if (query.getEndTime()!=null) {
+            queryWrapper.lt("entry_time", query.getEndTime());
+        }
+        // 6. businessId not null -> where business_id=#{}
+        if (query.getBusinessId()!=null) {
+            queryWrapper.eq("business_id", query.getBusinessId());
+        }
+        return queryWrapper;
     }
 }
--- lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/service/BusinessService.java
+++ lbs-basic/basic-web/src/main/java/com/tarena/lbs/basic/web/service/BusinessService.java
@@ -1,6 +1,7 @@
 package com.tarena.lbs.basic.web.service;
 
 import com.alibaba.nacos.common.utils.CollectionUtils;
+import com.github.pagehelper.PageInfo;
 import com.tarena.lbs.base.protocol.pager.PageResult;
 import com.tarena.lbs.basic.web.repository.BusinessRepository;
 import com.tarena.lbs.pojo.basic.po.BusinessPO;
@@ -20,26 +21,44 @@ public class BusinessService {
     public PageResult<BusinessVO> pageList(BusinessQuery query) {
         // 1. initialize voPages
         PageResult<BusinessVO> voPages = new PageResult<>();
-        voPages.setPageNo(query.getPageNo());
-        voPages.setPageSize(query.getPageSize());
-        // 2. voPages need 5 properties: total, objects, pageNo, pageSize, totalPages
-        // totalPage is calculated = total%pageSize==0?total/pageSize:total/pageSize+1
-        // 2.1 get total number, select count(*) from lbs_business
-        Long total = businessRepository.count(query);
-        // 2.2 get page objects, select * from lbs_business limit from, size
-        List<BusinessPO> pos = businessRepository.getBusinessByPage(query);
-        // 3 convert PO to VO
+        // 2. use PageHelper
+        PageInfo<BusinessPO> pageInfo = businessRepository.getPages(query);
+        // 3. set voPages
+        voPages.setPageNo(pageInfo.getPageNum());
+        voPages.setPageSize(pageInfo.getPageSize());
+        voPages.setTotal(pageInfo.getTotal());
+        // 4. pos need to nonempty, then convert to vo
         List<BusinessVO> vos = null;
-        if (CollectionUtils.isNotEmpty(pos)) {
-            // use stream() api (better than for loop)
-            vos = pos.stream().map(po -> {
+        if (CollectionUtils.isNotEmpty(pageInfo.getList())) {
+            vos = pageInfo.getList().stream().map(po -> {
                 BusinessVO vo = new BusinessVO();
                 BeanUtils.copyProperties(po, vo);
                 return vo;
             }).collect(Collectors.toList());
         }
-        voPages.setTotal(total);
         voPages.setObjects(vos);
         return voPages;
+        // Deprecated
+//        voPages.setPageNo(query.getPageNo());
+//        voPages.setPageSize(query.getPageSize());
+//        // 2. voPages need 5 properties: total, objects, pageNo, pageSize, totalPages
+//        // totalPage is calculated = total%pageSize==0?total/pageSize:total/pageSize+1
+//        // 2.1 get total number, select count(*) from lbs_business
+//        Long total = businessRepository.count(query);
+//        // 2.2 get page objects, select * from lbs_business limit from, size
+//        List<BusinessPO> pos = businessRepository.getBusinessByPage(query);
+//        // 3 convert PO to VO
+//        List<BusinessVO> vos = null;
+//        if (CollectionUtils.isNotEmpty(pos)) {
+//            // use stream() api (better than for loop)
+//            vos = pos.stream().map(po -> {
+//                BusinessVO vo = new BusinessVO();
+//                BeanUtils.copyProperties(po, vo);
+//                return vo;
+//            }).collect(Collectors.toList());
+//        }
+//        voPages.setTotal(total);
+//        voPages.setObjects(vos);
+//        return voPages;
     }
 }
